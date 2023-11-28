e2783bf305d135ac55c139e4b9e6687847e2bec1
ssefah <ssefahfarouq@gmail.com>
2023-11-28 14:39:16 +0300

    First 3 js TOP exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+/**
+ * Repeats a given word a specified number of times.
+ * @param {string} word - The word to be repeated.
+ * @param {number} times - The number of times the word should be repeated.
+ * @returns {string} - The resulting string that repeats the word parameter times number of times. If times is less than or equal to 0, it returns the string 'Error'.
+ */
+const repeatString = (word, times) => {
+  if (times <= 0) {
+    return 'Error';
+  }
 
+  let string = '';
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
